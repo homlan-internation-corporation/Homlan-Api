94d9358fc4f877130ed72a1958b0210a960321d8
matowang <wmatthew123@gmail.com>
2019-11-14 14:18:56 +0800

    mailing functionality

--- routes/email-form.js
+++ routes/email-form.js
@@ -1,8 +1,38 @@
+'use strict';
+const nodemailer = require('nodemailer');
+
 const express = require('express');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const pug = require('pug');
+
+const compileNewsRequestEmail = pug.compileFile('./views/news-request-email.pug');
+
+router.post('/', async (req, res) => {
     console.log(req.body);
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    let info = await transporter.sendMail({
+        from: `"Homlan.com" homlansmtp@gmail.com`, // sender address
+        to: process.env.EMAIL_RECIPIENT, // list of receivers
+        subject: 'News Letter Request', // Subject line
+        html: compileNewsRequestEmail({
+            email: req.body.email
+        }),
+    }, (err, info) => {
+        if (err)
+            console.log(err);
+        else
+            console.log(info);
+    });
+
     res.end();
 });
 
