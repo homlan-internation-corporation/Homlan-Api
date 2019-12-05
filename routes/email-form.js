6e7b326f24c408255f01003b26ef78fe9fa7f449
matowang <wmatthew123@gmail.com>
2019-12-05 13:56:11 +0800

    responsible error handling

--- routes/email-form.js
+++ routes/email-form.js
@@ -4,13 +4,31 @@ const nodemailer = require('nodemailer');
 const express = require('express');
 const router = express.Router();
 
-const pug = require('pug');
+const Joi = require('@hapi/joi');
+const formSchema = Joi.object({
+    name: Joi.string().required(),
+    company: Joi.string(),
+    email: Joi.string().email().required(),
+    phone: Joi.string(),
+    reason: Joi.string()
+})
 
+const pug = require('pug');
 const compileEmailForm = pug.compileFile('./views/email-form.pug');
 
+//Validate Format
 router.post('/', async (req, res, next) => {
     console.log(req.body);
+    const result = formSchema.validate(req.body);
+    if (result.error) {
+        res.status(422).send('Invalid Format')
+    } else {
+        next()
+    }
+})
 
+//Send Email
+router.post('/', async (req, res, next) => {
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -26,7 +44,8 @@ router.post('/', async (req, res, next) => {
         html: compileEmailForm(req.body),
     }, (err, info) => {
         if (err) {
-            next(err)
+            console.log(err);
+            res.status(500).send('Something broke!')
         } else {
             res.info = info
             next()
@@ -34,15 +53,10 @@ router.post('/', async (req, res, next) => {
     });
 })
 
-router.use(function (err, req, res, next) {
-    console.error(err);
-    res.status(500).send('Something broke!')
-    next()
-})
-
+//Success
 router.post('/', async (req, res) => {
     console.log(res.info);
-    res.send("success!")
+    res.send("Success!")
 });
 
 module.exports = router;
